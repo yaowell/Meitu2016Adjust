8be16d3f1e0a332280597db8efc1207a11341748
yaowell <280442219@qq.com>
2026-09-20 18:27:36 +0800

    更新 Meitu2016AdjustEngine.swift

--- Meitu2016AdjustEngine.swift
+++ Meitu2016AdjustEngine.swift
@@ -5,11 +5,11 @@ import Compression
 final class Meitu2016AdjustEngine {
     static let shared = Meitu2016AdjustEngine()
 
-    private static let lutCompressed = Data(base64Encoded: """
+    private static let lutCompressed: Data = Data(base64Encoded: """
 eNrtnfejZ1V1xS3UYSjD0EEQRDoiRQUUsVAUUATpoICGpoCOVCkGg0hoA0pg6DUYiQU1RCXEaNSoFCFEQBQYpM5QZhiYGYoUc885++x+zj33vTf8xJ+w3l77
 s/a973v3fsMb3/imN715vvnmX2CBBRdcaOGFxy2yyPjxiy622OJLLDFhwpITJy619NLLLLPscsstv8IKK6640lvesvIqq7z1rauuttrbVl/97W9fY80111p7
 7XXWWXe99dZ/xzs22OCdG2640UYbb7LJu9797ve8Z9PNNtv8ve993/u2eP/7t/zABz74wQ99+MNbbb31Ntts+5GPfHS77bbffoePfezjO+74iU/stPPOn9xl
-l1132233PfbYc6+99t5nn099+tP77rf//p/57Gf/7oADDzzo4EM+97nPH3rY4Yd/4YuTvvSlI4486uhjjv3ycccdf8KJX/n7k776Dyd/7ZSvn/qPp51+xlmT
+l1132233PfbYc6+99t5nn099+tP77rf//p/57Gf/7oADDzzo4EM+97nPH3rY4Yd/4YuTvvSlI4486uhjjv3cccdf8KJX/n7k776Dyd/7ZSvn/qPp51+xlmT
 zz7nG98897zzp1xw0cWXXHb5FVddfc23vn3td777/R/88N/+/Sc33Pizn//3r/7ndzf//vb/u+ue+x54ePpTz77wtzd0+t8M+hcC/YuC/iVB/7KgfyXQv+qq
 q70t6F9jjTXXCvrXXXe99YP+d74zyN94k3e9K8jfdLPNNw/yt3j/llsG+R/68FZbgfyPgvyPg/xP7rLrrrvtvvsee+651957J/n7MfmHBPmHBfmTQH6n38g/
 86zJQf4/nTflggsvuuTSy6648qp/vuZfvv2v3/nedT/40fU/jvp/+evf/O6W3//vH+7+031/eWT6jLL+xUH/UqB/+eVD+Vd6y8orh/JH/an8a0H514fybwzl
@@ -20,10 +20,11 @@ yf6/ualr/w5/90596NHHZ84u619C6Jftn+y/Btkfy7+RKH+Sv6Vwv2z+JD+5v4L+wzj6y81P7k/wy+Xv
 3P8B7f5K8/ehn9gn9Sf5XfnB/R38uvJf25X/+7H8QL/O/qH9O/y7+hd29GP7p/Kvou3fV/6C+4vsq6Ef2eeVP8qP0R/gJ8sf6Jf039K1/51/7OLvYV+/iT+l
 n9tflX9Dv/zK/aXBp4b+fvaR+0P0J/hB+VP4ZfvH9u/w/5eHpz3x9OwXy/pt+3P7y/BH+1fKr9zvsK+O/j72ofsBfl325fJn+sX069r/rns6/Y9Me3LU+pX9
 A7H7e/gXz7+cv1qACoCsG6A4gQ8VH9x+m27iG7wp8afkn7zClQAsLEBBAD1EzB/AXKiugqM4y/iX+Cv0f4+/uT4g/r/HzsYFkM=
-""")
+""")!
 
     private static let lut: [UInt8] = {
         var out = [UInt8](repeating: 0, count: 57600)
+
         let count = lutCompressed.withUnsafeBytes { raw in
             compression_decode_buffer(
                 &out,
@@ -34,6 +35,7 @@ A7H7e/gXz7+cv1qACoCsG6A4gQ8VH9x+m27iG7wp8afkn7zClQAsLEBBAD1EzB/AXKiugqM4y/iX+Cv0
                 COMPRESSION_ZLIB
             )
         }
+
         precondition(count == out.count)
         return out
     }()
@@ -44,13 +46,18 @@ A7H7e/gXz7+cv1qACoCsG6A4gQ8VH9x+m27iG7wp8afkn7zClQAsLEBBAD1EzB/AXKiugqM4y/iX+Cv0
         contrast: Double,
         sharpness: Double
     ) -> UIImage? {
-        guard let cg = image.cgImage else { return nil }
+        guard let cg = image.cgImage else {
+            return nil
+        }
 
         let width = cg.width
         let height = cg.height
         let rowBytes = width * 4
 
-        var data = [UInt8](repeating: 0, count: rowBytes * height)
+        var data = [UInt8](
+            repeating: 0,
+            count: rowBytes * height
+        )
 
         let space = CGColorSpaceCreateDeviceRGB()
 
@@ -61,7 +68,8 @@ A7H7e/gXz7+cv1qACoCsG6A4gQ8VH9x+m27iG7wp8afkn7zClQAsLEBBAD1EzB/AXKiugqM4y/iX+Cv0
             bitsPerComponent: 8,
             bytesPerRow: rowBytes,
             space: space,
-            bitmapInfo: CGImageAlphaInfo.premultipliedFirst.rawValue |
+            bitmapInfo:
+                CGImageAlphaInfo.premultipliedFirst.rawValue |
                 CGBitmapInfo.byteOrder32Big.rawValue
         ) else {
             return nil
@@ -92,12 +100,12 @@ A7H7e/gXz7+cv1qACoCsG6A4gQ8VH9x+m27iG7wp8afkn7zClQAsLEBBAD1EzB/AXKiugqM4y/iX+Cv0
             )
         }
 
-        guard let out = ctx.makeImage() else {
+        guard let output = ctx.makeImage() else {
             return nil
         }
 
         return UIImage(
-            cgImage: out,
+            cgImage: output,
             scale: image.scale,
             orientation: image.imageOrientation
         )
@@ -212,6 +220,11 @@ A7H7e/gXz7+cv1qACoCsG6A4gQ8VH9x+m27iG7wp8afkn7zClQAsLEBBAD1EzB/AXKiugqM4y/iX+Cv0
         _ data: inout [UInt8],
         offset: Int
     ) {
+        guard offset >= 0,
+              offset + 255 < Self.lut.count else {
+            return
+        }
+
         for i in stride(
             from: 1,
             to: data.count,
